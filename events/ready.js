2d7c8e6b3927c45c9a39d0f9d20cea5e336b6adc
` Just Fôrevêr <89777525+DevJustForever@users.noreply.github.com>
2022-02-01 16:25:04 +0300

    V1.0.0
    V1.0.0 Sürümü.

---
+++ events/ready.js
@@ -0,0 +1,14 @@
+module.exports = async (client) => {
+    await client.user.setPresence({ activities: [{ name: client.user.username + ' sizlerle!', url: "https://www.twitch.tv/mertcelik2196", type: "COMPETING" }], status: 'dnd', afk: false });
+    client.functions.log("Bot giriş yaptı.", "READY");
+    let usersCount = client.guilds.cache.reduce((a, b) => a + b.memberCount, 0);
+    let users2Count = client.users.cache.size;
+    let guild = client.guilds.cache.size;
+    let channel = client.channels.cache.size;
+    let emojis = client.emojis.cache.size;
+    client.functions.log(`Kullanıcı: [${usersCount}]`, "READY");
+    client.functions.log(`Kullanıcı: [${users2Count}]`, "READY");
+    client.functions.log(`Sunucu: [${guild}]`, "READY");
+    client.functions.log(`Kanal: [${channel}]`, "READY");
+    client.functions.log(`Emoji: [${emojis}]`, "READY");
+};
